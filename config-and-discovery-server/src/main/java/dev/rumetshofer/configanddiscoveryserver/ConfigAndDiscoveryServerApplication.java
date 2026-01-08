85f394a90df6dba4154fed45b0f4557c936d854c
Rene Rumetshofer <rene.rumetshofer@hotmail.com>
2026-01-08 19:54:23 +0100

    add gitignore and global proeprties

---
+++ config-and-discovery-server/src/main/java/dev/rumetshofer/configanddiscoveryserver/ConfigAndDiscoveryServerApplication.java
@@ -0,0 +1,17 @@
+package dev.rumetshofer.configanddiscoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableConfigServer
+@EnableEurekaServer
+@SpringBootApplication
+public class ConfigAndDiscoveryServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigAndDiscoveryServerApplication.class, args);
+    }
+
+}
---
+++ config-and-discovery-server/src/test/java/dev/rumetshofer/configanddiscoveryserver/ConfigAndDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.rumetshofer.configanddiscoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigAndDiscoveryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
